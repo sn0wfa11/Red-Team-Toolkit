5a6f08443c431f26e8c516fd80ba65de5803b08f
Josh Hale <jhale85446@gmail.com>
2018-03-22 23:06:15 -0500

    Work on builder

--- builder.py
+++ builder.py
@@ -10,14 +10,13 @@ except Exception, err:
   print >> sys.stderr, err
   sys.exit(1)
 
-global verbose, dns_lookup, pattern
+global verbose, dns_lookup, pattern, numbers
 
 templates = []
 numbers = []
 pattern = "@{N}"
 hosts = []
 ips = []
-passthrough = []
 
 def get_templates(filename):
   check_file(filename)
@@ -27,32 +26,41 @@ def get_templates(filename):
       if line != "":
         if pattern in line:
           templates.append(line)
-        else:
-          passthrough.append(line)
 
 def make_patterns():
   for number in numbers:
     for template in templates:
       hosts.append(template.replace(pattern, str(number)))
 
-def make_output():
+def build_range(in_string):
   output = []
+  in_string = in_string.strip()
+  sub_strings = in_string.split(",")
 
-  for number in numbers:
-    for host in hosts;
-      if number in host
+  for sub_string in sub_strings:
+    dash_range = sub_string.split("-")
+    if len(dash_range) == 1:
+      output.append(int(sub_string))
+    if len(dash_range) == 2:
+      num_range = number_range(int(dash_range[0]), int(dash_range[1]))
+      for num in num_range:
+        output.append(num)
 
+  return output 
 
-def out_to_file(path):
+def number_range(start, end):
+  output = []
+  if end < start:
+    temp = start
+    start = end
+    end = temp
 
-
-
-  write_list_to_file(path, hosts)
-
-
+  for x in range(start, end + 1):
+    output.append(x)
+  return output  
 
 def parse_args(args, parser):
-  global verbose, dns_lookup, pattern
+  global verbose, dns_lookup, pattern, numbers
 
   verbose = args.verbose
   dns_lookup = args.dns_lookup
@@ -61,7 +69,9 @@ def parse_args(args, parser):
     templates.append(args.template)
   if args.template_file:
     get_templates(args.template_file)
-
+  if args.range:
+    numbers = build_range(args.range)
+  
 def main(argv):
   parser = argparse.ArgumentParser(description = "Create a list of host names from CDC host templates.")
   parser.add_argument("-T", "--template_file", type = str, help = "File containing host name templates. One per line.")
@@ -69,17 +79,19 @@ def main(argv):
   parser.add_argument("-v", "--verbose", action = "store_true", help = "Verbose Reporting")
   parser.add_argument("-d", "--dns_lookup", action = "store_true", help = "Perform a DNS lookup and store IP address for each host.")
   parser.add_argument("-r", "--range", type = str, help = "Ranges of team numbers. Example 1,3,5-10,12")
+  parser.add_argument("-O", "--output_file", type = str, help = "File path to output the built results.")
   
   args = parser.parse_args()
   if len(argv) == 1:
     help_exit(parser)
   parse_args(args, parser)
-
-  numbers.append("1")
-  numbers.append("2")
-
+  
   make_patterns()
-  out_to_file("/root/Desktop/output.txt")
+  if args.output_file:
+    write_list_to_file(args.output_file, hosts)
+  else:
+    for line in hosts:
+      print line
 
 if __name__ == "__main__":
   main(sys.argv)
--- lib/logging.py
+++ lib/logging.py
@@ -1,9 +1,17 @@
 def write_list_to_file(filepath, in_list):
   try:
-    out_file = open(filepath, "a+")
+    out_file = open(filepath, "w+")
     for line in in_list:
       out_file.write(line + "\n")
     out_file.close()
   except IOError as e:
     print "I/O error({0}): {1}".format(e.errno, e.strerror)
 
+def apend_list_to_file(filepath, in_list):
+  try:
+    out_file = open(filepath, "a+")
+    for line in in_list:
+      out_file.write(line + "\n")
+    out_file.close()
+  except IOError as e:
+    print "I/O error({0}): {1}".format(e.errno, e.strerror)
